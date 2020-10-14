72acf038c8f4a14c49ec51cbd1067c716611e3cf
tsvetomira12 <cvetunka2001@abg.bg>
2020-10-14 14:46:14 +0000

    +

---
+++ src/exercise3/NamePrinter.java
@@ -0,0 +1,7 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class NumberCounter {
+  public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
---
+++ src/exercise3/PrimeNumber.java
@@ -0,0 +1,18 @@
+package exercise3;
+
+import java.util.Scanner;
+
+  public class NumberCounter {
+    public static void main (String[] args) {
+      Scanner input = new Scanner(System.in);
+
+        int n = next.Int();
+
+        if(n>2) {
+          prime = false
+          System.print.out("Primezahl");
+          break;
+        }else 
+        }
+        }
+  
